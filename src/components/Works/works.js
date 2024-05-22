d41e7d80eec8276139d22b415b4a6bd5e2718667
alrayanamer <alrayanamer20@gmail.com>
2024-05-21 23:34:11 -0500

    Instagram API

--- src/components/Works/works.js
+++ src/components/Works/works.js
@@ -1,31 +1,14 @@
 import React from 'react'
 import './works.css'
-import ComingSoon from '../../assets/comingSoon.jpg'
+import BeholdWidget from '@behold/react'
 
 const Works = () => {
-  const projects = [
-    { img: ComingSoon, alt: "ComingSoon", link: "", text: "Coming Soon" },
-    { img: ComingSoon, alt: "ComingSoon", link: "", text: "Coming Soon" },
-    { img: ComingSoon, alt: "ComingSoon", link: "", text: "Coming Soon" },
-    { img: ComingSoon, alt: "ComingSoon", link: "", text: "Coming Soon" },
-    { img: ComingSoon, alt: "ComingSoon", link: "", text: "Coming Soon" },
-    { img: ComingSoon, alt: "ComingSoon", link: "", text: "Coming Soon" },
-  ];
 
   return (
     <section id='works'>
       <h2 className="worksTitle">Gallery</h2>
       <span className="worksDesc">enter quick msg abt gallery</span>
-      <div className="worksImgs">
-        {projects.map((project, index) => (
-          <div key={index} className="worksItem">
-            <img src={project.img} alt={project.alt} className="worksImg" />
-          </div>
-        ))}
-      </div>
-      <a href="https://www.instagram.com/mozeemotorsports/" target="_blank" rel="noopener noreferrer">
-        <button className="worksBtn">See More</button>
-      </a>
+      <BeholdWidget feedId='UaeJTMCGan7Zdp7sQULr'/>
     </section>
   );
 }
