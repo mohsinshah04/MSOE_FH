447bfcea8360d08d43c6871924c67e2c01a185d8
AHABEEBULLAH04 <ahabeebullah18@gmail.com>
2024-08-10 18:03:26 -0500

    Revised text in Meet Mozee Motorsports section

--- src/components/About/about.js
+++ src/components/About/about.js
@@ -9,28 +9,34 @@ import AppDesign from '../../assets/app-design.png'
 const abouts = () => {
   return (
     <section id='about'>
-        <span className="aboutTitle">About Us</span>
-        <span className="aboutDesc">The Milwaukee School of Engineering (MSOE) SAE Formula Hybrid Team is a dynamic and innovative group dedicated to pushing the boundaries of automotive engineering. With a commitment to excellence and a collaborative spirit, we strive to foster hands-on learning experiences for our members while representing MSOE on an international stage. Through tireless dedication and a drive for innovation, we aim to foster the next generation of engineers and prepare them to contribute to the advancement of automotive technology.</span>
+        <span className="aboutTitle">What is Mozee Motorsports?</span>
+        <span className="aboutDesc">The Milwaukee School of Engineering (MSOE) SAE Formula Hybrid
+             Team is a dynamic and innovative group dedicated to pushing the boundaries of 
+             automotive engineering. With a commitment to excellence and a collaborative spirit,
+              we strive to foster hands-on learning experiences for our members while representing
+               MSOE on an international stage. Through tireless dedication and a drive for innovation,
+                we aim to foster the next generation of engineers and prepare them to contribute to the
+                 advancement of automotive technology.</span>
         <div className="aboutBars">
             <div className="aboutBar">
                 <img src={UIDesign} alt="UIDesign" className="aboutBarImg" />
                 <div className="aboutBarText">
                     <h2>Meet The Team</h2>
-                    <p>meet the team msg</p>
+                    <p>Our Leadership and Dedicated Team Members</p>
                 </div>
             </div>
             <div className="aboutBar">
                 <img src={WebDesign} alt="WebDesign" className="aboutBarImg" />
                 <div className="aboutBarText">
-                    <h2>Projects</h2>
-                    <p>msg about projects</p>
+                    <h2>About Us</h2>
+                    <p>A Detailed Description of Mozee Motorsports, Our Mision, and The Competitions We Compete In</p>
                 </div>
             </div>
             <div className="aboutBar">
                 <img src={AppDesign} alt="AppDesign" className="aboutBarImg" />
                 <div className="aboutBarText">
-                    <h2>The cars</h2>
-                    <p>sum abt how long you guys been around</p>
+                    <h2>The Cars</h2>
+                    <p>Stats and Fun Facts about the Cars in Mozee Motorsports' Hybrid Era</p>
                 </div>
             </div>
         </div>
